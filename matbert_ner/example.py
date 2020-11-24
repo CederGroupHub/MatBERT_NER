c014763616a4f13012006587373c830fa284b137
Amalie Trewartha <amalie.trewartha@gmail.com>
2020-11-23 22:42:08 -0800

    Changed scheduler and optimizer

--- matbert_ner/example.py
+++ matbert_ner/example.py
@@ -1,5 +1,5 @@
 from models.bert_model import BertNER
-from transformers import BertTokenizer, AutoConfig
+from transformers import BertTokenizer, AutoConfig, get_linear_schedule_with_warmup
 from utils.data import InputExample, convert_examples_to_features, collate_fn
 from utils.dataloader import load_data
 import torch
@@ -8,6 +8,7 @@ import torch.optim as optim
 from tqdm import tqdm
 
 batch_size = 20
+n_epochs = 5
 
 device = "cuda"
 
@@ -23,10 +24,34 @@ tensor_dataset = load_data(datafile, tokenizer)
 dataloader = DataLoader(tensor_dataset, batch_size=batch_size,
                         shuffle=True, num_workers=0)
 
-optimizer = optim.AdamW(ner_model.parameters(), lr=0.01)
+
+no_decay = ["bias", "LayerNorm.weight"]
+bert_parameters = ner_model.bert.named_parameters()
+classifier_parameters = ner_model.classifier.named_parameters()
+bert_lr = 5e-5
+classifier_lr = 5e-5
+optimizer_grouped_parameters = [
+    {"params": [p for n, p in bert_parameters if not any(nd in n for nd in no_decay)],
+     "weight_decay": 0.0,
+     "lr": bert_lr},
+    {"params": [p for n, p in bert_parameters if any(nd in n for nd in no_decay)],
+     "weight_decay": 0.0,
+     "lr": bert_lr},
+
+    {"params": [p for n, p in classifier_parameters if not any(nd in n for nd in no_decay)],
+     "weight_decay": 0.0,
+     "lr": classifier_lr},
+    {"params": [p for n, p in classifier_parameters if any(nd in n for nd in no_decay)],
+     "weight_decay": 0.0,
+     "lr": classifier_lr}
+]
+optimizer = optim.AdamW(optimizer_grouped_parameters, 5e-5, eps=1e-8)
+scheduler = get_linear_schedule_with_warmup(
+    optimizer, num_warmup_steps=0, num_training_steps=n_epochs*len(dataloader)/(batch_size)
+)
 
 
-for epoch in range(4):
+for epoch in range(n_epochs):
     print("\n\n\nEpoch: " + str(epoch + 1))
     for i, batch in enumerate(tqdm(dataloader)):
         
@@ -39,14 +64,15 @@ for epoch in range(4):
         loss, predicted = ner_model.forward(**inputs)
         loss.backward()
         optimizer.step()
+        scheduler.step()
 
         if i%100 == 0:
             labels = inputs['labels']
             predicted = torch.max(predicted,-1)[1]
 
             true = torch.where(labels > 0, labels, 0)
-            predicted = torch.where(true > 0, predicted, -1)
+            predicted = torch.where(labels > 0, predicted, -1)
 
-            true = torch.where(true > 0, true==predicted, False).sum().item()/torch.count_nonzero(true)
+            acc = (true==predicted).sum().item()/torch.count_nonzero(true)
 
-            print("loss: {}, acc: {}".format(loss.item(),true.item()))
+            print("loss: {}, acc: {}".format(loss.item(),acc.item()))
