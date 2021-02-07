24af99ff265edffb70bcac00e7ffff2376d62f99
walkernr <walker.nr256@gmail.com>
2021-02-07 11:25:27 -0600

    remove prints

--- matbert_ner/models/base_ner_model.py
+++ matbert_ner/models/base_ner_model.py
@@ -90,16 +90,16 @@ class NERModel(ABC):
                 # prediction_tags = [[self.classes[ii] for ii, jj in zip(i, j) if jj != -100] for i, j in zip(prediction_list, labels_list)]
                 # valid_tags = [[self.classes[ii] for ii in i if ii != -100] for i in labels_list]
 
-                for a, b, c, d, i, j in zip(list(inputs['input_ids'].cpu().numpy()),
-                                            list(inputs['labels'].cpu().numpy()),
-                                            list(inputs['valid_mask'].cpu().numpy()),
-                                            list(inputs['attention_mask'].cpu().numpy()),
-                                            prediction_list,
-                                            valid_attention_mask):
-                    print(len(a), len(b), len(c), len(d), len(i), len(j))
-                    print('ID', '\t', 'L', '\t', 'VM', '\t', 'AM', '\t', 'P', '\t', 'VAM')
-                    for aa, bb, cc, dd, ii, jj in zip(a, b, c, d, i, j):
-                        print(aa, '\t', bb, '\t', cc, '\t', dd, '\t', ii, '\t', jj)
+                # for a, b, c, d, i, j in zip(list(inputs['input_ids'].cpu().numpy()),
+                #                             list(inputs['labels'].cpu().numpy()),
+                #                             list(inputs['valid_mask'].cpu().numpy()),
+                #                             list(inputs['attention_mask'].cpu().numpy()),
+                #                             prediction_list,
+                #                             valid_attention_mask):
+                #     print(len(a), len(b), len(c), len(d), len(i), len(j))
+                #     print('ID', '\t', 'L', '\t', 'VM', '\t', 'AM', '\t', 'P', '\t', 'VAM')
+                #     for aa, bb, cc, dd, ii, jj in zip(a, b, c, d, i, j):
+                #         print(aa, '\t', bb, '\t', cc, '\t', dd, '\t', ii, '\t', jj)
                 
                 prediction_tags = [[self.classes[ii] for ii, jj, kk in zip(i, j, k) if kk==1 and jj>=0] for i, j, k in zip(prediction_list, labels_list, valid_attention_mask)]
                 label_tags = [[self.classes[ii] for ii, jj in zip(i, j) if jj==1 and ii>=0] for i, j in zip(labels_list, valid_attention_mask)]
@@ -207,7 +207,10 @@ class NERModel(ABC):
                     for j in range(max_len):
                         if inputs['valid_mask'][i][j].item() == 1:
                             jj += 1
-                            valid_attention_mask[i, jj] = inputs['attention_mask'][i][j].item()
+                            if inputs['input_ids'][i][j].item() in [101, 102]:
+                                valid_attention_mask[i, jj] = 0
+                            else:
+                                valid_attention_mask[i, jj] = inputs['attention_mask'][i][j].item()
                 valid_attention_mask = list(valid_attention_mask)
 
                 # prediction_tags = [[self.classes[ii] for ii, jj in zip(i, j) if jj != -100] for i, j in zip(prediction_list, labels_list)]
