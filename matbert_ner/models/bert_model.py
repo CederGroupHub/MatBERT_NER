fcdddcf228aa0a52604cafe2980d3328b53ee1be
walkernr <walker.nr256@gmail.com>
2021-02-13 14:09:54 -0600

    imp_value change

--- matbert_ner/models/bert_model.py
+++ matbert_ner/models/bert_model.py
@@ -201,7 +201,7 @@ class CRF(nn.Module):
                                              'I': 'IE'}
     
 
-    def init_crf_transitions(self, imp_value=-0.1):
+    def init_crf_transitions(self, imp_value=-1.0):
         num_tags = len(self.tag_names)
         # penalize bad beginnings and endings
         for i in range(num_tags):
