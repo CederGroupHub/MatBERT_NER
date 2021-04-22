a74c895303ed0891cce2a024c7e21f4dd233a39c
walkernr <walker.nr256@gmail.com>
2021-04-22 14:53:55 -0500

    bert scaling

--- matbert_ner/example.py
+++ matbert_ner/example.py
@@ -7,8 +7,8 @@ import json
 import torch
 import numpy as np
 
-# seeds = [2**x for x in np.arange(16)]
-seeds = [256]
+seeds = [2**x for x in np.arange(16)]
+# seeds = [256]
 torch.backends.cudnn.deterministic = True
 
 datafiles = {'solid_state': 'data/ner_annotations.json',
@@ -16,10 +16,10 @@ datafiles = {'solid_state': 'data/ner_annotations.json',
              'impurityphase': 'data/impurityphase_fullparas.json',
              'aunpmorph': 'data/aunpmorph_annotations_fullparas.json'}
 
-# splits = np.arange(10, 85, 5)
-splits = [80]
-# tag_schemes = ['IOBES']
-tag_schemes = ['IOB2']
+splits = np.arange(10, 85, 5)
+# splits = [80]
+tag_schemes = ['IOBES']
+# tag_schemes = ['IOB2']
 n_epochs = 16
 lr = 2e-4
 
@@ -29,15 +29,15 @@ models = {'bilstm': 'bert-base-uncased',
           'scibert': 'allenai/scibert_scivocab_uncased',
           'matbert': '/home/amalie/MatBERT_NER/matbert_ner/matbert-base-uncased'}
 
-model_names = ['matbert']
-# data_names = ['aunpmorph', 'doping', 'solid_state']
-data_names = ['solid_state']
+model_names = ['bert']
+data_names = ['solid_state', 'doping', 'aunpmorph']
+# data_names = ['solid_state']
 
 for model_name in model_names:
     for data in data_names:
         for seed in seeds:
             torch.manual_seed(seed)
-            configs = {'_{}_full_crf_{}_{}_{}'.format(data, tag_scheme, seed, split): {'full_finetuning': True, 'format': tag_scheme, 'split': [split/100, split/800, 0.1]} for split in splits for tag_scheme in tag_schemes}
+            configs = {'_{}_full_crf_{}_{}_{}'.format(data, tag_scheme.lower(), seed, split): {'full_finetuning': True, 'format': tag_scheme, 'split': [split/100, split/800, 0.1]} for split in splits for tag_scheme in tag_schemes}
             for alias, config in configs.items():
                 save_dir = os.getcwd()+'/{}_results{}/'.format(model_name, alias)
                 try:
