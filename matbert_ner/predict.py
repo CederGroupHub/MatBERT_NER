9e9b9a8c5f4a908d3b7e03282371d0314500fcd5
ardunn <ardunn@lbl.gov>
2021-07-31 20:12:20 -0700

    add some docs and more complete bugfixes

--- matbert_ner/predict.py
+++ matbert_ner/predict.py
@@ -2,12 +2,29 @@ import os
 
 
 import torch
-from utils.data import NERData
+from matbert_ner.utils.data import NERData
 from matbert_ner.models.bert_model import BERTNER
 from matbert_ner.models.model_trainer import NERTrainer
 
 
-def predict(texts_tokenized, model_file, state_path, scheme="IOBES", batch_size=256, device="cpu", seed=None):
+def predict(texts, model_file, state_path, scheme="IOBES", batch_size=256, device="cpu", seed=None):
+    """
+    Predict labels for texts. Please limit input to 512 tokens or less.
+
+    Args:
+        texts ([str]): List of string texts to predict labels for. Limit to 512 estimated tokens. Untokenized text will be tokenized interally with
+            the Materials Tokenizer.
+        model_file (str): Path to BERT model file.
+        state_path (str): Path to model state for NER task, fine tuned for specific task (e.g., gold nanoparticles).
+        scheme (str): IOBES or IOB2.
+        batch_size (int): Number of samples to predict in one batch pass.
+        device (str): Select 'cpu', 'gpu', or torch specific logic for running on multiple GPUs.
+        seed (int, None): Seed for prediction.
+
+    Returns:
+        ([dict]): dictionaries of tokens and label annotations
+
+    """
     split_dict = {'predict': 1.0}
 
     if 'gpu' in device:
@@ -26,8 +43,15 @@ def predict(texts_tokenized, model_file, state_path, scheme="IOBES", batch_size=
     torch.backends.cudnn.benchmark = False
     torch.backends.cudnn.deterministic = True
 
+
+    input_formatted_texts = []
+    for t in texts:
+        entry = {"text": t, "meta": {"par": 0}, "doi": ""}
+        input_formatted_texts.append(entry)
+
+
     ner_data = NERData(model_file, scheme=scheme)
-    ner_data.preprocess(texts_tokenized, split_dict, is_file=False, annotated=False, sentence_level=False, shuffle=False, seed=seed)
+    ner_data.preprocess(input_formatted_texts, split_dict, is_file=False, annotated=False, sentence_level=False, shuffle=False, seed=seed)
     ner_data.create_dataloaders(batch_size=batch_size, shuffle=False, seed=seed)
     bert_ner = BERTNER(model_file=model_file, classes=ner_data.classes, scheme=scheme, seed=seed)
     bert_ner_trainer = NERTrainer(bert_ner, device)
