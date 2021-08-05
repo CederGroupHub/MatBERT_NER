9e071ca2b8b82044d2bee69b89468e1f53456985
walkernr <walker.nr256@gmail.com>
2021-08-04 21:08:37 -0500

    cleaned mongo entries before dataloader

--- matbert_ner/predict_mongo.py
+++ matbert_ner/predict_mongo.py
@@ -55,10 +55,10 @@ print(100*'=')
 dois = [d['meta']['doi'] for d in db.matbert_ner_entries_walkernr_test_v2.find()]
 
 i = 0
-splits = 0
 ner_data = NERData(model_file, scheme=scheme)
 for entries in grouper(fetch_batch_size, db.entries.find({'doi': {'$nin': dois}})):
-    ner_data.preprocess(entries, split_dict, is_file=False, annotated=False, sentence_level=False, shuffle=False, seed=seed)
+    entries_clean = [{'meta': {'doi': entry['doi'], 'par': 0}, 'text': '{}. {}'.format(entry['title'], entry['abstract'])} for entry in entries]
+    ner_data.preprocess(entries_clean, split_dict, is_file=False, annotated=False, sentence_level=False, shuffle=False, seed=seed)
     ner_data.create_dataloaders(batch_size=int(np.ceil(len(ner_data.data['predict'])/4)), shuffle=False, seed=seed)
     if i ==0:
         bert_ner = BERTNER(model_file=model_file, classes=ner_data.classes, scheme=scheme, seed=seed)
@@ -66,14 +66,13 @@ for entries in grouper(fetch_batch_size, db.entries.find({'doi': {'$nin': dois}}
         bert_ner_trainer.load_state(state_path=state_path, optimizer=False)
         labels = list(set(ner_data.classes))
     annotations = bert_ner_trainer.predict(ner_data.dataloaders['predict'], original_data=ner_data.data['predict'])
-    for entry, annotation in tqdm(zip(entries, annotations), desc='| updating entry user/model/date stamps |'):
+    for entry, annotation in tqdm(zip(entries_clean, annotations), desc='| updating entry user/model/date stamps |'):
         entry.update(annotation)
         entry.update({'user': 'walkernr', 'model': model_reference, 'date': datetime.now().strftime('%Y:%m:%d:%H:%M:%S')})
-    db.matbert_ner_entries_walkernr_test_v2.insert_many(entries)
+    db.matbert_ner_entries_walkernr_test_v2.insert_many(entries_clean)
     print(100*'=')
     print('Entries Written to DB')
     print(100*'=')
     i += 1
     if i == 8:
         break
-print(splits)
