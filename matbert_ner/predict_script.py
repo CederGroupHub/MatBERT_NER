db1b49088d03d88b4f0e7e70c3c05d4f7d2c2f32
walkernr <walker.nr256@gmail.com>
2021-09-26 11:37:29 -0500

    mem fix

--- matbert_ner/predict_script.py
+++ matbert_ner/predict_script.py
@@ -5,5 +5,5 @@ model = '../../matbert-base-uncased'
 solid_state_state = './matbert_solid_state_paragraph_iobes_crf_10_lamb_5_1_012_1e-04_2e-03_1e-02_0e+00_exponential_256_100/best.pt'
 doping_state = './matbert_doping_paragraph_iobes_crf_10_lamb_5_1_012_1e-04_2e-03_1e-02_0e+00_exponential_256_100/best.pt'
 
-predict(data, True, model, solid_state_state, predict_path=solid_state_state.replace('best.pt', 'predict_doping_solid_state_169828.pt'), device='gpu:0')
+# predict(data, True, model, solid_state_state, predict_path=solid_state_state.replace('best.pt', 'predict_doping_solid_state_169828.pt'), device='gpu:0')
 predict(data, True, model, doping_state, predict_path=doping_state.replace('best.pt', 'predict_doping_doping_169828.pt'), device='gpu:0')
